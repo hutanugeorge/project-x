8ee39aa14243517ee377730977075dfbcf788097
Alex Hutanu <valexhutanu@gmail.com>
2022-03-19 21:31:59 +0200

    Added username in navbar and added logout behaviour to logout button in navbar

---
+++ src/components/NavBar/interfaces.ts
@@ -0,0 +1,5 @@
+export interface DecodedToken {
+   userID: string
+   firstName: string
+   lastName: string
+}
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -7,7 +7,7 @@ import MessageIcon from '../../icons/MessageIcon'
 export default () => {
    return <div className="post">
       <div className="post__header">
-         <p className="post__header__person">
+         <div className="post__header__person">
             <div className="post__header__person__photo">
                <img
                   src="https://alexanderklebe.com/wp-content/uploads/Headshot_photographer_actors_Berlin_Joerg.jpg"
@@ -15,10 +15,10 @@ export default () => {
             </div>
             &nbsp;
             Costica 'Titi' Bernard
-         </p>
-         <p className="post__header__more-options">
+         </div>
+         <div className="post__header__more-options">
             <SettingsIcon/>
-         </p>
+         </div>
       </div>
       <div className="post__content">
          <img
@@ -27,17 +27,17 @@ export default () => {
       </div>
       <div className="post__actions">
          <div className="post__actions__left">
-            <p className="post__actions__like">
+            <div className="post__actions__like">
                <LikeIcon/>
-            </p>
-            <p className="post__actions__comment">
+            </div>
+            <div className="post__actions__comment">
                <MessageIcon/>
-            </p>
+            </div>
          </div>
          <div className="post__actions__right">
-            <p className="post__actions__save">
+            <div className="post__actions__save">
                <SaveIcon/>
-            </p>
+            </div>
          </div>
       </div>
    </div>
