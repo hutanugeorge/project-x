d3abbe33e4dee260214d0bfe06964d6c83390da5
Alex Hutanu <valexhutanu@gmail.com>
2022-03-20 16:34:16 +0200

    Added tests for Label component

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -2,7 +2,7 @@ import { LabelProps } from './interface'
 
 
 export default ({ htmlFor, text, error }: LabelProps) => {
-   return <label className={`label ${error ? 'label__error' : ''}`} htmlFor={htmlFor}>
+   return <label data-testid="label" className={`label ${error ? 'label__error' : ''}`} htmlFor={htmlFor}>
       {error ?? text}
    </label>
 }
---
+++ src/components/Label/__tests__/Label.test.tsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom'
+
+import { render, screen } from '@testing-library/react'
+import Label from '../Label'
+
+
+describe('Label component testing', () => {
+   test('render label', () => {
+      const div = document.createElement('div')
+      ReactDOM.render(<Label htmlFor={'labelText'} text={'labelText'} error={'input__error'}/>, div)
+   })
+   test('check for Label props', () => {
+      render(<Label htmlFor={'labelText'} text={'labelText'} error={undefined}/>)
+      const label = screen.getByTestId('label')
+      expect(label.getAttribute('for')).toBe('labelText')
+      expect(label.textContent).toBe('labelText')
+      expect(label.className).toBe('label ')
+   })
+   test('check for Label props with error', () => {
+      render(<Label htmlFor={'labelText'} text={'labelText'} error={'Error'}/>)
+      const label = screen.getByTestId('label')
+      expect(label.getAttribute('for')).toBe('labelText')
+      expect(label.textContent).toBe('Error')
+      expect(label.className).toContain('label__error')
+   })
+})
