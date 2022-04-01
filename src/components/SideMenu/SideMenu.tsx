84e637368489cd2cf5b8af7f50724b70aa89a1de
Alex Hutanu <valexhutanu@gmail.com>
2022-04-01 20:13:00 +0300

    Refactored comment component

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,8 +1,4 @@
 import * as React from 'react'
-<<<<<<< HEAD
-=======
-import { Scrollbars } from 'react-custom-scrollbars'
->>>>>>> 61bb196b740fdb0ad3ce04b62b5837ff1ff0f970
 
 interface SideMenuProps {
    title: string
@@ -13,13 +9,7 @@ export default ({ children, title }: SideMenuProps) => {
    return (
       <div className="side-menu">
          <h2 className="side-menu__title">{title}</h2>
-<<<<<<< HEAD
          <div className="side-menu__content">{children}</div>
-=======
-         {/*<Scrollbars style={{ height: 650 }}>*/}
-         <div className="side-menu__content">{children}</div>
-         {/*</Scrollbars>*/}
->>>>>>> 61bb196b740fdb0ad3ce04b62b5837ff1ff0f970
       </div>
    )
 }
