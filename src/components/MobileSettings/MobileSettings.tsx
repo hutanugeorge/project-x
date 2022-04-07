0dedeafe19aaa3626cc0388025dcfe1159889793
Alex Hutanu <valexhutanu@gmail.com>
2022-04-07 16:19:03 +0300

    Created settings section for mobile view & fixed post form

--- src/components/MobileSettings/MobileSettings.tsx
+++ src/components/MobileSettings/MobileSettings.tsx
@@ -5,34 +5,40 @@ import LogoutIcon from '../../icons/LogoutIcon'
 export default () => {
    return <div className='mobile-settings'>
       <div className='mobile-settings__header'>
-         <p className='mobile-settings__header__title'>
-            Settings
+         <div className='mobile-settings__header__user-photo'>
+            <img src='https://swashvillage.org/storage/img/images_2/william-biography_20.jpg'
+                 alt='user-photo' />
+         </div>
+         <p className='mobile-settings__header__username'>
+            William
          </p>
       </div>
       <div className='mobile-settings__content'>
-         <div className='mobile-settings__content__settings1'>
-            <p className='mobile-settings__content__settings1__name'>Settings1</p>
-            <ArrowRightIcon />
-         </div>
-         <div className='mobile-settings__content__settings1'>
-            <p className='mobile-settings__content__settings1__name'>Settings2</p>
-            <ArrowRightIcon />
-         </div>
-         <div className='mobile-settings__content__settings1'>
-            <p className='mobile-settings__content__settings1__name'>Settings3</p>
-            <ArrowRightIcon />
-         </div>
-         <div className='mobile-settings__content__settings1'>
-            <p className='mobile-settings__content__settings1__name'>Settings4</p>
-            <ArrowRightIcon />
-         </div>
-         <div className='mobile-settings__content__settings1'>
-            <p className='mobile-settings__content__settings1__name'>Settings5</p>
-            <ArrowRightIcon />
-         </div>
-         <div className='mobile-settings__content__settings1'>
-            <p className='mobile-settings__content__settings1__name'>Settings6</p>
-            <ArrowRightIcon />
+         <div className='mobile-settings__content__settings'>
+            <div className='mobile-settings__content__settings__settings1'>
+               <p className='mobile-settings__content__settings__settings1__name'>Settings1</p>
+               <ArrowRightIcon />
+            </div>
+            <div className='mobile-settings__content__settings__settings1'>
+               <p className='mobile-settings__content__settings__settings1__name'>Settings2</p>
+               <ArrowRightIcon />
+            </div>
+            <div className='mobile-settings__content__settings__settings1'>
+               <p className='mobile-settings__content__settings__settings1__name'>Settings3</p>
+               <ArrowRightIcon />
+            </div>
+            <div className='mobile-settings__content__settings__settings1'>
+               <p className='mobile-settings__content__settings__settings1__name'>Settings4</p>
+               <ArrowRightIcon />
+            </div>
+            <div className='mobile-settings__content__settings__settings1'>
+               <p className='mobile-settings__content__settings__settings1__name'>Settings5</p>
+               <ArrowRightIcon />
+            </div>
+            <div className='mobile-settings__content__settings__settings1'>
+               <p className='mobile-settings__content__settings__settings1__name'>Settings6</p>
+               <ArrowRightIcon />
+            </div>
          </div>
          <div className='mobile-settings__content__log-out' onClick={() => {
             localStorage.removeItem('token')
